748d335a2d609a08a5dcb2c1f8940b4616309a5d
demo-pc <ThorLabsPC@email.com>
2025-05-21 16:23:35 +0400

    Added LeCroy measurement script.

--- analysis/main_measure.py
+++ analysis/main_measure.py
@@ -7,30 +7,33 @@ import time
 
 def main():
     filepath = 'traces.hdf5'
-    nb_of_samples = 4_900_000
-    nb_of_traces = 50
+    nb_of_keys = 2 # One key suffices for SPA, DPA, and CPA. For template attacks, two keys are needed.
+    nb_of_samples = 20_000
+    nb_of_traces = 10_000
 
-    #scope = TeledyneLeCroyScope(int16_not_int8=True, float_not_int=False)
+    scope = TeledyneLeCroyScope(int16_not_int8=True, float_not_int=False)
     cw308 = CW308_STM32F4_AES()
     cw308.test_correctness()
 
-    #cw308.write_hash(cw308.random_32_bytes())
-    #secret_scalar_k = cw308.random_32_bytes_with_order_check()
-    #cw308.write_secret_scalar_k(secret_scalar_k)
-
-    #with h5py.File(filepath, 'w') as f:
-    #    f.create_dataset("trigger", shape=(nb_of_samples), dtype=np.int16)
-    #    f.create_dataset("power", shape=(nb_of_traces, nb_of_samples), dtype=np.int16)
-    #    f.create_dataset("scalar_k", shape=(32), dtype=np.uint8)
-    #    f["scalar_k"][:] = np.frombuffer(secret_scalar_k, dtype=np.uint8)
-    #    print('Collecting traces...')
-    #    for i in tqdm(range(nb_of_traces)):
-    #        scope.arm_single_trace()
-    #        time.sleep(0.5)
-    #        cw308.sign()
-    #        time.sleep(0.5)
-    #        f["power"][i,:] = scope.get_single_trace(channel='C3', nb_of_samples = nb_of_samples)
-    #    f["trigger"][:] += scope.get_single_trace(channel='C1', nb_of_samples = nb_of_samples)
+    print('Collecting AES traces...')
+    with h5py.File(filepath, 'w') as f:
+        f.create_dataset("trigger", shape=(nb_of_samples), dtype=np.int16)
+        f.create_dataset("keys", shape=(nb_of_keys, 16), dtype=np.uint8)
+        f.create_dataset("plaintexts", shape=(nb_of_keys, nb_of_traces, 16), dtype=np.uint8)
+        f.create_dataset("power", shape=(nb_of_keys, nb_of_traces, nb_of_samples), dtype=np.int16)
+        for i in range(nb_of_keys):
+            key = np.random.randint(0, high=256, size=16, dtype=np.uint8)
+            f["keys"][i,:] = key
+            cw308.write_key(key.tobytes())
+            for j in tqdm(range(nb_of_traces)):
+                plaintext = np.random.randint(0, high=256, size=16, dtype=np.uint8)
+                f["plaintexts"][i,:] = plaintext
+                cw308.write_plaintext(plaintext.tobytes())
+                scope.arm_single_trace()
+                time.sleep(0.05)
+                cw308.encrypt()
+                f["power"][i,j,:] = scope.get_single_trace(channel='C3', nb_of_samples = nb_of_samples)
+        f["trigger"][:] = scope.get_single_trace(channel='C1', nb_of_samples = nb_of_samples)
 
 if __name__ == '__main__':
     main()
